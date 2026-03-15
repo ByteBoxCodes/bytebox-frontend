ee3c0be864c12656b85723c0d0f5fe67f6f35466
Mohit Singh <mohitsinghx12@gmail.com>
2026-03-16 01:34:15 +0530

    updated the o auth

--- src/features/auth/GoogleLoginAuth.tsx
+++ src/features/auth/GoogleLoginAuth.tsx
@@ -3,19 +3,22 @@ import axios from "axios";
 
 export default function GoogleLoginAuth() {
     return (
-        <GoogleLogin
-            onSuccess={(response) => {
-                const idToken = response.credential;
+        <div className="w-full flex justify-center items-center">
+            <GoogleLogin
+                width={400}
+                onSuccess={(response) => {
+                    const idToken = response.credential;
 
-                axios.post("http://localhost:8080/api/users/google", { idToken: idToken })
-                    .then((response) => {
-                        console.log(response);
-                        localStorage.setItem("token", response.data.data);
-                        window.location.href = "/problems";
-                    })
+                    axios.post("https://api.byteboxcodes.com/api/users/google", { idToken: idToken })
+                        .then((response) => {
+                            console.log(response);
+                            localStorage.setItem("token", response.data.data);
+                            window.location.href = "/problems";
+                        })
 
-            }}
-            onError={() => console.log('Login Failed')}
-        />
+                }}
+                onError={() => console.log('Login Failed')}
+            />
+        </div>
     )
 };
