72f24529c829db8e7757011c59bb99115b183c66
Mohit Singh <mohitsinghx12@gmail.com>
2026-03-23 03:11:37 +0530

    updated the toast

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
           <RouterProvider router={router} />
-          <Toaster richColors position="top-right" />
+          <Toaster position="bottom-right" />
         </ThemeProvider>
       </QueryClientProvider>
     </GoogleOAuthProvider>
