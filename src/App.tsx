70d10140f072847e9227b1bee75ff3a91c6bcaf8
Mohit Singh <mohitsinghx12@gmail.com>
2026-04-01 10:57:24 +0530

    updated the footer for landing page only

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
 import { cleanupExpiredStorage } from "./utils/storageCleanup";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
       <Header />
       <main className="flex-1 min-h-0 overflow-y-auto relative scroll-smooth">
         <Outlet />
-        <Footer />
       </main>
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/layout/Footer";
 import HeroSection from "../features/landing/HeroSection";
 
 export default function HomePage() {
-    return (
-        <div>
-            <HeroSection />
-        </div>
-    )
+  return (
+    <div>
+      <HeroSection />
+      <Footer />
+    </div>
+  );
 }
